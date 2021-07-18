27595711f80a8e27871ca44b137eed35b4c6c633
kevinkeven <kevinkeven20@gmail.com>
2021-07-18 17:56:50 +0300

    new

--- users/forms.py
+++ users/forms.py
@@ -30,7 +30,26 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('first_name', 'last_name','username', 'email', 'age',)
 
 class CustomUserChangeForm(UserChangeForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('username'),
+            Row(
+                Column(Field('first_name')),
+                Column(Field('last_name')),
+            ),
+            Field('email'),
+            Field('age'),
+            Field('password1'),
+            Field('password2'),
+
+            Submit('submit', 'Create Account', css_class="btn btn-warning btn-block")
+            
+        )
+
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name','username', 'email', 'age',)
+        fields = ('first_name', 'last_name', 'email', 'age',)
 
